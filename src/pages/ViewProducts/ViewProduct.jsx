0eb0be768b6f95db331f3598190b92b3710095ac
hain <hainofficialweb@gmail.com>
2025-07-14 16:49:54 +0530

    merchant panel 2.0

--- src/pages/ViewProducts/ViewProduct.jsx
+++ src/pages/ViewProducts/ViewProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { BASE_URL, IMG_URL } from '../Urls/Urls';
